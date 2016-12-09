1d65df070b51036f2432ba1890ff349cf75afa7f
Klaudio Milankovic <klaudio.milankovic@am2studio.hr>
2016-12-09 10:34:40 +0100

    Ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+     url: 'https://www.codeschool.com/users/am2studio.json',
+     dataType: 'jsonp',
+     success: function(response) {
+          var badges = response.courses.completed;
+          console.log('badges');
+          addCourses(badges);
+        }
+   });
+
+   function addCourses(courses) {
+
+     var $badges_container = $("#badges");
+       courses.forEach(function(course) {
+         var $course = $('<div />', {
+           'class': 'course'
+         }).appendTo($badges_container);
+
+         $('<h3 />', {
+           text: course.title
+         }).appendTo($course);
+
+         $('<img />',{
+           src: course.badge
+         }).appendTo($course)
+
+         $('<a />', {
+           'class': 'btn btn-primary',
+           target: '_blank',
+           href: course.url,
+           text: 'See Course'
+         }).appendTo($course)
+
+    
+       });
+   }
 
 });
